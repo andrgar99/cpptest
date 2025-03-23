ccf82c2aa51d06d163bca5a31007a994b5b613ea
andrgar99 <rikkeach@gmail.com>
2025-03-23 11:22:24 +0100

    removed code

--- include/header.h
+++ include/header.h
@@ -9,13 +9,9 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <boost/lexical_cast.hpp>
 
 
 void print();
-void computevector(std::vector<float>& data);
 
-void byteswap_vector_gpu(std::vector<int16_t>& data);
-void byteswap_vector(std::vector<int16_t>& data);
 
 #endif
